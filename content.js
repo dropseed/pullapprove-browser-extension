55b97c90fa6378d5632ef05f89d849ac40d93e94
Dave Gaeddert <dave.gaeddert@gmail.com>
2021-01-14 11:19:26 -0600

    Comment next steps

--- content.js
+++ content.js
@@ -6,34 +6,42 @@ import { getURLParameterByName } from "./components/utils";
 const pr = document.querySelector(".merge-pr");
 if (!pr) return;
 
+// should find any with context name being pullapprove or pullapprove-company? no slashes allowed
+
 const links = pr.querySelectorAll(
   "a[href^='https://app.pullapprove.com/report/']"
 );
-if (links.length < 1) return;
 
-// could be more?
-const reportURL = links[0].href;
-const jsonURL = getURLParameterByName(reportURL, "url");
-// use fingerprint to cache local? or does browser cover that?
+// create divs for each sidebar
+// pass through the node it should observe for url changes, etc.
+// it does the fetching (componentDidMount and observe change)
 
-const currentUsername = document.querySelector("meta[name='user-login']")
-  .content;
+links.forEach(link => {
 
-fetch(jsonURL)
-  .then((response) => response.json())
-  .then((data) => {
-    console.log(data);
-    // Add our own div outside of the sidebar partial, but inside the container which
-    // does the actual layout sizing
-    const pullapproveContainer = document.createElement("div");
-    const githubSidebar = document.querySelector("#partial-discussion-sidebar");
-    githubSidebar.parentNode.prepend(pullapproveContainer);
-    ReactDOM.render(
-      <Sidebar
-        currentUsername={currentUsername}
-        reportData={data}
-        reportURL={reportURL}
-      />,
-      pullapproveContainer
-    );
-  });
+    const reportURL = link.href;
+    const jsonURL = getURLParameterByName(reportURL, "url");
+    // use fingerprint to cache local? or does browser cover that?
+
+    const currentUsername = document.querySelector("meta[name='user-login']")
+        .content;
+
+    fetch(jsonURL)
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data);
+            // Add our own div outside of the sidebar partial, but inside the container which
+            // does the actual layout sizing
+            const pullapproveContainer = document.createElement("div");
+            const githubSidebar = document.querySelector("#partial-discussion-sidebar");
+            githubSidebar.parentNode.prepend(pullapproveContainer);
+            ReactDOM.render(
+                <Sidebar
+                    currentUsername={currentUsername}
+                    reportData={data}
+                    reportURL={reportURL}
+                />,
+                pullapproveContainer
+            );
+        });
+
+})
